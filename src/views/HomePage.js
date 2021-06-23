928ad915bfc06c0eaab1334781ba7c8456005fff
Yaroslava Musiichuk <iaroslava1607@gmail.com>
2021-06-23 23:53:52 +0300

    Refactor the code

--- src/views/HomePage.js
+++ src/views/HomePage.js
@@ -1,16 +1,16 @@
 import { Component } from "react";
 import axios from "axios";
 
+const URL_Top_Movies =
+  "https://api.themoviedb.org/3/trending/all/day?api_key=7f5c22cc3d00862e5e2a46a3605db11a";
+
 class HomePage extends Component {
   state = {
     topMovies: [],
   };
 
   async componentDidMount() {
-    const response = await axios.get(
-      "https://api.themoviedb.org/3/trending/all/day?api_key=7f5c22cc3d00862e5e2a46a3605db11a"
-    );
-    console.log(response.data.results);
+    const response = await axios.get(URL_Top_Movies);
 
     this.setState({ topMovies: response.data.results });
   }
@@ -20,10 +20,10 @@ class HomePage extends Component {
       <>
         <h1>Trending today</h1>
         <ul>
-          {this.state.topMovies.map((movie) => (
-            <li key={movie.id}>
-              {movie.original_title}
-              {movie.original_name}
+          {this.state.topMovies.map(({ id, original_name, original_title }) => (
+            <li key={id}>
+              {original_title}
+              {original_name}
             </li>
           ))}
         </ul>
